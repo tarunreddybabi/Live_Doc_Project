5045d87498c61f435b71e1c4d3d2d3502423f453
rakesh reddy <tarunreddybabireddy@gmail.com>
2024-10-22 16:47:34 +0530

    new features

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -11,6 +11,11 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import UserTypeSelector from "./UserTypeSelector";
+import Collaborator from "./Collaborator";
+import { updateDocumentAccess } from "@/lib/actions/room.actions";
 
 const ShareModal = ({
   roomId,
@@ -25,7 +30,19 @@ const ShareModal = ({
 
   const [userType, setUserType] = useState<UserType>("viewer");
 
-  const shareDocumentHandler = async () => {};
+  const shareDocumentHandler = async () => {
+    setLoading(true);
+
+    await updateDocumentAccess({
+      roomId,
+      email,
+      userType: userType as UserType,
+      updatedBy: user.info,
+    });
+
+    setLoading(false);
+  };
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger>
@@ -43,14 +60,46 @@ const ShareModal = ({
           <p className="mr-1 hidedn sm:block">Share</p>
         </Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="shad-dialog">
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle>Manage who can view this project</DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            Select which users can view and edit this document
           </DialogDescription>
         </DialogHeader>
+        <Label htmlFor="email" className="mt-6 text-blue-100">
+          Email address
+        </Label>
+        <div className="flex items-center gap-3">
+          <div className="flex flex-1 rounded-md bg-dark-400">
+            <Input
+              id="email"
+              placeholder="Enter email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="share-input"
+            />
+            <UserTypeSelector userType={userType} setUserType={setUserType} />{" "}
+          </div>
+          <Button type="submit" onClick={shareDocumentHandler} className="gradient-blue flex h-full gap-1 px-5" disabled={loading}>
+            {loading ? 'Sending...' : 'Invite'}
+          </Button>
+        </div>
+
+        <div className="my-2 space-y-2">
+          <ul className="flex flex-col">
+            {collaborators.map((collaborator) => (
+              <Collaborator
+                key={collaborator.id}
+                roomId={roomId}
+                creatorId={creatorId}
+                email={collaborator.email}
+                collaborator={collaborator}
+                user={user.info}
+              />
+            ))}
+          </ul>
+        </div>
       </DialogContent>
     </Dialog>
   );
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -2,7 +2,7 @@
 import { nanoid } from "nanoid";
 import { liveblocks } from "../liveblocks";
 import { revalidatePath } from "next/cache";
-import { parseStringify } from "../utils";
+import { getAccessType, parseStringify } from "../utils";
 
 export const createDocument = async ({
   userId,
@@ -72,5 +72,49 @@ export const getDocuments = async (email: string) => {
     return parseStringify(rooms);
   } catch (error) {
     console.log(`Error happened while getting a rooms ${error}`);
+  }
+};
+
+export const updateDocumentAccess = async ({
+  roomId,
+  email,
+  userType,
+  updatedBy,
+}: ShareDocumentParams) => {
+  try {
+    const usersAccesses: RoomAccesses = {
+      [email]: getAccessType(userType) as AccessType,
+    };
+    const room = await liveblocks.updateRoom(roomId, { usersAccesses });
+    if (room) {
+    }
+    revalidatePath(`/documents${roomId}`);
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error happened while updating a room access: ${error}`);
+  }
+};
+
+export const removeCollaborator = async ({
+  roomId,
+  email,
+}: {
+  roomId: string;
+  email: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+    if (room.metadata.email === email) {
+      throw new Error("You cannot remove yourself from the document");
+    }
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      usersAccesses: {
+        [email]: null,
+      },
+    });
+    revalidatePath(`/documents${roomId}`);
+    return parseStringify(updatedRoom);
+  } catch (error) {
+    console.log(`Error happened while removing a collaborator: ${error}`);
   }
 };
